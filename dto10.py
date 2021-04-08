d9715c1ec154310a5fada23284ce006c4c2e78a5
Александр Семин <a.semin@atol.ru>
2021-04-08 15:36:20 +0300

    Добавил вывод номер ККТ в магазине

--- dto10.py
+++ dto10.py
@@ -62,6 +62,12 @@ class DTO10:
         return self.fptr.errorCode()
 
 
+    def get_logical_number_kkt(self):
+        self.fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_STATUS)
+        self.fptr.queryData()
+        return self.fptr.getParamInt(IFptr.LIBFPTR_PARAM_LOGICAL_NUMBER)
+
+
     def get_model_information_kkt(self):
         self.fptr.setParam(IFptr.LIBFPTR_PARAM_DATA_TYPE, IFptr.LIBFPTR_DT_MODEL_INFO)
         self.fptr.queryData()
